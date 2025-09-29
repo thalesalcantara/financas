fe4eff194f82519f5d1ea2adbd7f09fe45ce1cf3
Coopex Entregas <thalesalcantara88@gmail.com>
2025-09-29 18:39:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from sqlalchemy import delete as sa_delete, text as sa_text, func
-from sqlalchemy import delete as sa_delete, func
 
 
 # =========================
