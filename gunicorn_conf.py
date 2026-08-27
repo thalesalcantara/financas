7485a0d8b1cfd39da361f9688b092bff1b6e7f40
Coopex Entregas <thalesalcantara88@gmail.com>
2026-08-27 12:18:56 -0300

    chore: carregar correcao v12 no deploy

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import logging
 
 log = logging.getLogger("gunicorn.error")
-BUILD_VERSION = "2026-08-07.1416"
+BUILD_VERSION = "2026-08-27.1205-v12"
 
 
 def post_worker_init(worker):
@@ -20,8 +20,7 @@ def post_worker_init(worker):
 
         callbacks = flask_app.after_request_funcs.get(None, [])
         flask_app.after_request_funcs[None] = [
-            callback
-            for callback in callbacks
+            callback for callback in callbacks
             if getattr(callback, "__name__", "") != "coopex_upgrade_after_request"
         ]
 
@@ -47,93 +46,19 @@ def post_worker_init(worker):
         import admin_preserve_v9  # noqa: F401
         import admin_v10_fix  # noqa: F401
         import admin_v10_hotfix  # noqa: F401
+        import permission_swap_fix_v12  # noqa: F401
 
         if "coopex_build_probe" not in flask_app.view_functions:
             from flask import jsonify
-
             @flask_app.get("/__coopex_build", endpoint="coopex_build_probe")
             def coopex_build_probe():
-                return jsonify(
-                    ok=True,
-                    build=BUILD_VERSION,
-                    production_scale=True,
-                    exact_scale_date=True,
-                    exact_shift_end=True,
-                    multiple_daily_shifts=True,
-                    previous_day_submission=True,
-                    restaurant_week_pending=True,
-                    restaurant_pending_without_inner_scroll=True,
-                    restaurant_current_shift_only=True,
-                    restaurant_approval_tab=True,
-                    restaurant_reject_and_lock=True,
-                    rejected_coop_resubmit_blocked=True,
-                    rejected_rest_manual_launch_allowed=True,
-                    approved_value_creates_admin_launch=True,
-                    approval_returns_main_dashboard=True,
-                    approval_no_second_launch_required=True,
-                    approval_named_confirmation=True,
-                    admin_launch_live_sync=True,
-                    admin_launch_partial_replacement_disabled=True,
-                    admin_actions_persistent=True,
-                    admin_horizontal_v9=True,
-                    admin_menu_matches_restaurant=True,
-                    admin_lightweight_design=True,
-                    admin_full_desktop=True,
-                    admin_summary_default_today=True,
-                    admin_summary_live_table=True,
-                    admin_launch_full_filters=True,
-                    admin_launch_deductions_visible=True,
-                    admin_launch_full_edit=True,
-                    admin_scale_weekly_mode_preserved=True,
-                    admin_scale_add_person=True,
-                    admin_scale_separate_light_page=True,
-                    admin_swaps_separate_light_page=True,
-                    admin_scale_history_separate_light_page=True,
-                    admin_documents_separate_light_page=True,
-                    admin_legacy_modes_preserved=True,
-                    admin_config_preserved=True,
-                    admin_systems_tab_removed=True,
-                    admin_ratings_lifetime=True,
-                    cooperative_rating_lifetime=True,
-                    cooperative_password_edit=True,
-                    cooperative_username_edit=True,
-                    cooperative_photo_db_route=True,
-                    cooperative_phone_db_field=True,
-                    inactive_cooperatives_operationally_hidden=True,
-                    archived_cooperatives_history_preserved=True,
-                    admin_tab_prefetch_disabled=True,
-                    normal_financial_deductions_for_approved_launch=True,
-                    restaurant_alarm_every_5_minutes=True,
-                    cooperative_submission_sound=True,
-                    cooperative_new_rating_sound_once=True,
-                    cooperative_timeline_on_demand=True,
-                    restaurant_horizontal_dashboard=True,
-                    horizontal_menu_enforced=True,
-                    old_sidebar_disabled=True,
-                    optimized_panel_queries=True,
-                    indexed_scale_queries=True,
-                    launch_preview_limit=2,
-                    extra_production_tab=False,
-                    admin_v10=True,
-                    brl_display=True,
-                    brl_jinja_global=True,
-                    establishments_monday_to_sunday=True,
-                    finance_tabs_lazy=True,
-                    establishments_light=True,
-                    cooperatives_json_500_fixed=True,
-                    scale_partial_name_search=True,
-                    scale_xlsx_upload_preserved=True,
-                    swaps_compact_summary=True,
-                    blitz_cnh_crlv=True,
-                )
+                return jsonify(ok=True, build=BUILD_VERSION, admin_v12=True, granular_permissions=True, swap_request_restored=True, swap_admin_actions=True)
 
         @flask_app.after_request
         def coopex_build_header(response):
             response.headers["X-COOPEX-Build"] = BUILD_VERSION
             return response
 
-        log.info("Admin V10 consolidado e leve carregado. Build %s", BUILD_VERSION)
+        log.info("Admin V12 carregado. Build %s", BUILD_VERSION)
     except Exception:
-        log.exception(
-            "Melhorias complementares não carregaram; mantendo o aplicativo principal disponível."
-        )
+        log.exception("Melhorias complementares não carregaram; mantendo o aplicativo principal disponível.")
